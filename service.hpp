468622d8a5fa7cc71b4650d7acafdf763fe087e9
jayjae <wigglegigglegoggle@gmail.com>
2018-06-10 03:27:55 +0000

    Add actions

--- service.hpp
+++ service.hpp
@@ -1,31 +1,35 @@
 #pragma once
 
-#include <eosiolib/multi_index.hpp>
+#include <eosiolib/eosio.hpp>
 #include <whing/level.hpp>
 #include <whing/energy.hpp>
 #include <whing/flap.hpp>
+#include <whing/board_db.hpp>
+#include <whing/article_db.hpp>
+#include <whing/reply_db.hpp>
+
 namespace whing {
-   class service {
+   class service : public eosio::contract {
       public:
+         using contract::contract;
+
+         /// @abi action 
+         void add_board(account_name owner, std::string url);
+
+         /// @abi action
+         void add_article(uint32_t board_id, account_name owner, std::string url, std::string title, std::string data, std::string img_url);
+
+         /// @abi action
+         void add_reply(uint32_t article_id, account_name owner, std::string data);
+
+/*         
+         get_all_data_by_board_id(uint32_t board_id);
+
+         get_joined_boards();
+
+         get_owned_boards();
+*/
+   };
 
 
-
-
-      private:
-         struct board {
-            uint32_t       board_id; // pseudo-random
-            std::string    url;
-            uint64_t       exp;
-            uint32_t       level; // user's level, exp/EXP_THRESHOLD
-            account_name   owner;
-            uint64_t       num_of_articles;
-
-
-            uint64_t primary_key() const { return board_id; }
-            uint64_t secondary_key() const { return owner; }
-
-         };
-
-      };
-
-} // end of namespace whing
+} 
